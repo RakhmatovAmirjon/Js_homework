4b3ade2cf38ff7e18bc7166cc4747cea78e0e978
= <=>
2023-11-16 15:53:36 +0500

    message

---
+++ index.js
@@ -0,0 +1,136 @@
+let arr = [
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+]
+
+let categories = [
+    {
+        course: ' SMM',
+        count: 0
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0
+    },
+    {
+        course: '     3DSMAX',
+        count: 0
+    },
+    {
+        course: ' DIZAYN',
+        count: 0
+    },
+    {       
+        course: '   BACKEND',
+        count: 0
+    },
+]
+
+do {
+    name = prompt("Введите свою фамилию на английском и с маленькими буквами")
+} while(name !== "sharifkulov")
+
+
+for (let human of arr) {
+    let faculity = human.info.faculity.toUpperCase().trim()
+
+    for (let categoriya of categories) {
+      if (categoriya.course.trim().toUpperCase() === faculity) {
+        categoriya.count++;
+      }
+    }
+  }
+
+
+document.write(`
+Люди записавшиеся на ${categories[0].course} - ${categories[0].count} человека <br/>
+Люди записавшиеся на ${categories[1].course} - ${categories[1].count} человека <br/>
+Люди записавшиеся на ${categories[2].course} - ${categories[2].count} человека <br/> 
+Люди записавшиеся на ${categories[3].course} - ${categories[3].count} человека <br/> 
+Люди записавшиеся на ${categories[4].course} - ${categories[4].count} человека`)
